ba92f58d4c97fc9daeb92d751f6ef28933575e4f
Paul de Lange <p.delange@uky.edu>
2020-10-11 11:26:07 +0200

    preparejs

---
+++ js/prepare.sh
@@ -0,0 +1,12 @@
+npm init -y
+cp /Users/pauldelange/dev/setup/.eslintrc.js ./.eslintrc.js
+
+npm install --save-dev eslint \
+eslint-config-prettier \
+eslint-config-standard \
+eslint-plugin-import \
+eslint-plugin-node \
+eslint-plugin-prettier \
+eslint-plugin-promise \
+eslint-plugin-standard \
+prettier
